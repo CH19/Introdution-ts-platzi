04851a6f4fe7219c35a5408054a47a4068d4995f
CH19 <chcc190505@gmail.com>
2023-08-27 20:05:46 -0400

    clase 4 ts

---
+++ distribucion/013-any.js
@@ -0,0 +1,14 @@
+"use strict";
+(() => {
+    // el valor any es cualquier cosa es como volver a js
+    let myvar = 'sfsd';
+    myvar = 34;
+    // con esto quitamos el sistema de tipos de typescript pero es una mala practica
+    // el objetivo del any es para migracion de proyectos en js a ts ya que si tenemos un error repetitivo o algo asi lo podemos usar
+    // cast
+    // es transformar una variable any a un tipo en especifico para que sea tradatado de un tipo pero lo podemos usar
+    const rta = myvar.toUpperCase;
+    myvar = 'Christian';
+    console.log('Hola mundo');
+    console.log(myvar);
+})();
---
+++ src/012-arrays.ts
@@ -0,0 +1,13 @@
+(()=>{
+  // los arrays en ts tienen solo un tipo
+  // un array puede tener varios tipos de dstos pero hay que declararlos
+const number = [2,3,4, true, 'hola'];
+number.push(23);
+// Puede tener varios tipos inferidos del dato del Array
+number.push(true);
+// si ponemos un dato que no esta en el array da error
+// si queremos un array con valores propuesto lo deebes colocar así
+let coleccion: (number | string)[] = []
+coleccion.push(4);
+coleccion.push('34');
+})();
